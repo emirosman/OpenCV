7b083965ddf4cd7ec934dbb94e9309da5e32839d
emirosman <m.emirosman@gmail.com>
2016-10-22 00:09:35 +0300

    renk takip

---
+++ CMakeFiles/renk_takip2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/renk_takip2.dir/renk_takip2.cpp.o"
+  "renk_takip2.pdb"
+  "renk_takip2"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/renk_takip2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
